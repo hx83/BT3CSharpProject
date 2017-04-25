e960a632800cc5da8707719854653dfb8ef75c9f
Tommy <12530425@qq.com>
2017-04-25 22:17:52 +0800

    init

---
+++ TestBehavior.cs
@@ -0,0 +1,23 @@
+﻿using Behavior3CSharp;
+using Behavior3CSharp.Core;
+using LitJson;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBehavior : MonoBehaviour {
+
+    private BehaviorTree _tree;
+	// Use this for initialization
+	void Start () {
+        JsonData data = B3Config.Load("TestBehavior3");
+        _tree = new BehaviorTree();
+        _tree.Load(data);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (_tree != null)
+            _tree.Tick();
+	}
+}
